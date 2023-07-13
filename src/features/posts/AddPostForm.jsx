b8a72bca28afc48d4f4bfeb691c7d8f094eddfc8
kartos-1993 <1993kartos@gmail.com>
2023-07-13 16:35:20 +0545

    refractor: add prepare method in postAdded reducer

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,4 +1,3 @@
-import { nanoid } from "@reduxjs/toolkit";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postAdded } from "./postsSlice";
@@ -13,13 +12,7 @@ const AddPostForm = () => {
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content,
-        })
-      );
+      dispatch(postAdded(title, content));
       setTitle("");
       setContent("");
     }
