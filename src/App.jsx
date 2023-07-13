6fe7fa1da8e926dc7023d233ec4e1adfc36ace1d
kartos-1993 <1993kartos@gmail.com>
2023-07-13 16:02:04 +0545

    style:change min height to 100vh

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import AddPostForm from "./features/posts/AddPostForm";
 function App() {
   return (
     <div
-      className={`bg-slate-100 dark:bg-slate-800 dark:text-white w-full flex justify-center`}
+      className={`bg-slate-100 dark:bg-slate-800 dark:text-white w-full min-h-screen flex justify-center`}
     >
       <div className=" basis-3/5 max-w-md">
         <NavBar />
